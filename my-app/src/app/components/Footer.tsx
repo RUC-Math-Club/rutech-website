f0dc929940577ff78fbd8a64f728fa93ecaa28e5
DavidMajomi <77590768+DavidMajomi@users.noreply.github.com>
2025-10-17 12:52:18 -0400

    chore: add website footer

---
+++ my-app/src/app/components/Footer.tsx
@@ -0,0 +1,14 @@
+// Footer.jsx
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="text-gray-200 text-center py-4 mt-10">
+      <p className="text-sm">
+        Built by <span className="font-semibold text-blue-600">David Majomi</span> and the team at <span className="font-semibold text-blue-600">RuTech</span>
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,6 +1,7 @@
 import Navbar from './components/Navbar';
 import Main from './components/Main'
 import Overview from './components/Overview';
+import Footer from './components/Footer'
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       <Main />
       <div className="h-px bg-white" />
       <Overview />
+      <Footer />
     </>
  );
 }
